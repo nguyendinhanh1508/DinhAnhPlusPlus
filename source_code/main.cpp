ae031a104e5ac4eb0773a49980424bab1e461214
nguyendinhanh1508 <nguyendinhanh.that@gmail.com>
2025-03-30 23:00:36 +0800

    Update main.cpp
    allow to have blank lines

--- source_code/main.cpp
+++ source_code/main.cpp
@@ -2,18 +2,19 @@
 #include <vector>
 #include "DataTypes.h"
 #include "Tokenizer.h"
-#include "AST.h"
+#include "Parser.h"
 
 int main() {
     while (true) {
         std::string input;
         std::getline(std::cin, input);
+        if (input.empty()) continue;
         if (input == "EXIT") break;
         std::vector<Token> tokens = tokenize(input);
         size_t index = 0;
         AST_NODE* root = parse_language(tokens, index);
         if (tokens[index].type != END) {
-            std::cerr << "Syntax error: Line did not end properly" << std::endl;
+            std::cerr << "Syntax error: Failed to parse code" << std::endl;
             FREE_AST(root);
             continue;
         }
