3d6d48e1779fd7bb13d390bea196e7d2c4a4d9db
nguyendinhanh1508 <nguyendinhanh.that@gmail.com>
2025-04-02 22:06:06 +0800

    Update main_for_script.cpp
    fixed main_for_script file to adapt to the new tokenizer

--- source_code/main_for_script.cpp
+++ source_code/main_for_script.cpp
@@ -13,10 +13,12 @@ int main(int argc, char* argv[]) {
         if (input.empty()) continue;
         if (input == "EXIT") break;
         std::vector<Token> tokens = tokenize(input);
+        if (tokens.empty()) continue;
+        if (tokens.size() == 1 && tokens.back().type == END) continue;
         size_t index = 0;
         AST_NODE* root = parse_language(tokens, index);
         if (tokens[index].type != END) {
-            std::cerr << "Syntax error: Failed to parse code" << std::endl;
+            std::cerr << "Syntax Error: Failed to parse code" << std::endl;
             FREE_AST(root);
             continue;
         }
