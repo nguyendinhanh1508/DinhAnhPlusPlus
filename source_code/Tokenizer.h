9ac0e00078f5e8a66d7f813bd18ee184b46541c7
nguyendinhanh1508 <nguyendinhanh.that@gmail.com>
2025-04-04 21:25:13 +0800

    Update Tokenizer.h
    tokenizes function calls + changed string from "str" to "string"

--- source_code/Tokenizer.h
+++ source_code/Tokenizer.h
@@ -5,27 +5,37 @@
 #include "Converters.h"
 
 std::vector<Token> tokenize(const std::string& input) {
-    std::vector<Token> tokens;
+    tokens.clear();
     size_t i = 0;
     while (i < input.size()) {
         if (isspace(input[i])) { //ignore spaces, we need to replace this for indentations in the future
             i++;
             continue;
         }
+        else if (input[i] == ';') {
+            i++;
+            continue;
+        }
         else if (input[i] == '[') { //check for indices
-            tokens.push_back({ GET_VALUE, 0, 0, {}, "" });
+            push({ GET_VALUE, 0, 0, {}, "" });
             i++;
         }
         else if (input[i] == ']') {
-            tokens.push_back({ INDEX_END, 0, 0, {}, "" });
+            push({ INDEX_END, 0, 0, {}, "" });
             i++;
         }
-        else if (input[i] == '{') { //check for lists
-            tokens.push_back({ CURLY_LEFT });
+        else if (input[i] == '{') { //check for lists/function body
+            push({ CURLY_LEFT });
             i++;
         }
-        else if (input[i] == '}') { //out of the list
-            tokens.push_back({ CURLY_RIGHT });
+        else if (input[i] == '}') { //out of the list/function body
+            if (in_list == 0 && in_function_body == 1) {
+                in_function_body--;
+                function_body[cur_function_name] = cur_function_body;
+                cur_function_name.clear();
+                cur_function_body.clear();
+            }
+            else push({ CURLY_RIGHT });
             i++;
         }
         else if (isdigit(input[i])) { //check for integers
@@ -33,10 +43,10 @@ std::vector<Token> tokenize(const std::string& input) {
             while (i < input.size() && isdigit(input[i])) {
                 num += input[i++];
             }
-            tokens.push_back({ INTEGER, 0, stoi(num), {}, "" });
+            push({ INTEGER, 0, stoi(num), {}, "" });
         }
         else if (input[i] == ',') {
-            tokens.push_back({ COMMA });
+            push({ COMMA });
             i++;
         }
         else if (input[i] == '\"') {
@@ -46,7 +56,7 @@ std::vector<Token> tokenize(const std::string& input) {
                 str.push_back({ CHAR, 0, input[i++], {} });
             }
             if (i < input.size() && input[i] == '\"') {
-                tokens.push_back({ STRING, 0, 0, str, "" });
+                push({ STRING, 0, 0, str, "" });
                 i++;
             }
             else {
@@ -65,7 +75,7 @@ std::vector<Token> tokenize(const std::string& input) {
                 exit(1);
             }
             if (i < input.size() && input[i] == '\'') {
-                tokens.push_back({ CHAR, string_to_char(str), 0, {}, "" });
+                push({ CHAR, string_to_char(str), 0, {}, "" });
                 i++;
             }
             else {
@@ -78,66 +88,70 @@ std::vector<Token> tokenize(const std::string& input) {
             while (i < input.size() && (isalnum(input[i]) || input[i] == '_')) {
                 variable += input[i++];
             }
-            if (variable == "out") tokens.push_back({ OUTPUT, 0, 0, {}, "output" });
-            else if (variable == "in") tokens.push_back({ INPUT, 0, 0, {}, "input" });
-            else if (variable == "getline") tokens.push_back({ GETLINE, 0, 0, {}, "input" });
-            else if (variable == "nextline") tokens.push_back({ CHAR, '\n', 0, {}, "" });
+            if (variable == "out") push({ OUTPUT, 0, 0, {}, "output" });
+            else if (variable == "in") push({ INPUT, 0, 0, {}, "input" });
+            else if (variable == "getline") push({ GETLINE, 0, 0, {}, "input" });
+            else if (variable == "nextline") push({ CHAR, '\n', 0, {}, "" });
             else if (variable == "int") {
-                tokens.push_back({ NEW_VAR, 0, 0, {}, "create" });
-                tokens.push_back({ INTEGER_IDENTIFIER, 0, 0, {}, "integer" });
+                push({ NEW_VAR, 0, 0, {}, "create" });
+                push({ INTEGER_IDENTIFIER, 0, 0, {}, "integer" });
             }
             else if (variable == "char") {
-                tokens.push_back({ NEW_VAR, 0, 0, {}, "create" });
-                tokens.push_back({ CHAR_IDENTIFIER, 0, 0, {}, "character" });
+                push({ NEW_VAR, 0, 0, {}, "create" });
+                push({ CHAR_IDENTIFIER, 0, 0, {}, "character" });
             }
-            else if (variable == "str") {
-                tokens.push_back({ NEW_VAR, 0, 0, {}, "create" });
-                tokens.push_back({ STRING_IDENTIFIER, 0, 0, {}, "string" });
+            else if (variable == "string") {
+                push({ NEW_VAR, 0, 0, {}, "create" });
+                push({ STRING_IDENTIFIER, 0, 0, {}, "string" });
             }
             else if (variable == "list") {
-                tokens.push_back({ NEW_VAR, 0, 0, {}, "create" });
-                tokens.push_back({ LIST_IDENTIFIER, 0, 0, {}, "list" });
+                push({ NEW_VAR, 0, 0, {}, "create" });
+                push({ LIST_IDENTIFIER, 0, 0, {}, "list" });
             }
             else if (variable == "bool") {
-                tokens.push_back({ NEW_VAR, 0, 0, {}, "create" });
-                tokens.push_back({ BOOLEAN_IDENTIFIER, 0, 0, {}, "boolean" });
+                push({ NEW_VAR, 0, 0, {}, "create" });
+                push({ BOOLEAN_IDENTIFIER, 0, 0, {}, "boolean" });
+            }
+            else if (variable == "func") {
+                push({ NEW_VAR, 0, 0, {}, "create" });
+                push({ FUNCTION_IDENTIFIER, 0, 0, {}, "function" });
             }
             else if (variable == "true") {
-                tokens.push_back({ BOOLEAN, 0, 1, {}, "" });
+                push({ BOOLEAN, 0, 1, {}, "" });
             }
             else if (variable == "false") {
-                tokens.push_back({ BOOLEAN, 0, 0, {}, "" });
+                push({ BOOLEAN, 0, 0, {}, "" });
             }
-            else tokens.push_back({ IDENTIFIER, 0, 0, {}, variable });
+            else push({ IDENTIFIER, 0, 0, {}, variable });
         }
         else if (input[i] == '>') { //check for > and >=
             if (i < input.size() - 1 && input[i + 1] == '=') {
                 i++;
-                tokens.push_back({ MORE_EQUAL, 0, 0, {}, ">=" });
+                push({ MORE_EQUAL, 0, 0, {}, ">=" });
             }
-            else tokens.push_back({ MORE, 0, 0, {}, ">" });
+            else push({ MORE, 0, 0, {}, ">" });
             i++;
         }
         else if (input[i] == '<') { //check for < and <=
             if (i < input.size() - 1 && input[i + 1] == '=') {
                 i++;
-                tokens.push_back({ LESS_EQUAL, 0, 0 , {}, "<=" });
+                push({ LESS_EQUAL, 0, 0 , {}, "<=" });
             }
-            else tokens.push_back({ LESS, 0, 0, {}, "<" });
+            else push({ LESS, 0, 0, {}, "<" });
             i++;
         }
         else if (input[i] == '=') { //check for == and =
             if (i < input.size() - 1 && input[i + 1] == '=') {
                 i++;
-                tokens.push_back({ EQUAL, 0, 0, {}, "==" });
+                push({ EQUAL, 0, 0, {}, "==" });
             }
-            else tokens.push_back({ ASSIGN, 0, 0, {}, "=" });
+            else push({ ASSIGN, 0, 0, {}, "=" });
             i++;
         }
         else if (input[i] == '!') { //check for !=
             if (i < input.size() - 1 && input[i + 1] == '=') {
                 i++;
-                tokens.push_back({ NOT_EQUAL, 0, 0, {}, "!=" });
+                push({ NOT_EQUAL, 0, 0, {}, "!=" });
             }
             else {
                 std::cerr << "Syntax Error: Stray '!' found" << std::endl;
@@ -146,30 +160,30 @@ std::vector<Token> tokenize(const std::string& input) {
             i++;
         }
         else if (input[i] == '(') { //check for (
-            tokens.push_back({ LEFT_PARENTHESIS, 0, 0, {}, "(" });
+            push({ LEFT_PARENTHESIS, 0, 0, {}, "(" });
             i++;
         }
         else if (input[i] == ')') { //check for )
-            tokens.push_back({ RIGHT_PARENTHESIS, 0, 0, {}, ")" });
+            push({ RIGHT_PARENTHESIS, 0, 0, {}, ")" });
             i++;
         }
         else if (input[i] == '+') { //check for +
-            tokens.push_back({ PLUS, 0, 0, {}, "+" });
+            push({ PLUS, 0, 0, {}, "+" });
             i++;
         }
         else if (input[i] == '-') { //check for -
-            tokens.push_back({ MINUS, 0, 0, {}, "-" });
+            push({ MINUS, 0, 0, {}, "-" });
             i++;
         }
         else if (input[i] == '*') { //check for *
-            tokens.push_back({ MULTIPLY, 0, 0, {}, "*" });
+            push({ MULTIPLY, 0, 0, {}, "*" });
             i++;
         }
         else if (input[i] == '/') { //check for /
-            tokens.push_back({ DIVIDE, 0, 0, {}, "/" });
+            push({ DIVIDE, 0, 0, {}, "/" });
             i++;
         }
     }
-    tokens.push_back({ END, 0, 0, {}, "" });
+    push({ END, 0, 0, {}, "" });
     return tokens;
 }
