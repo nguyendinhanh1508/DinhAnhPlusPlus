74a1f4208fc34263970f4718a159e2ac0a103845
nguyendinhanh1508 <nguyendinhanh.that@gmail.com>
2025-04-05 16:31:51 +0800

    Update Values.h

--- src/Values.h
+++ src/Values.h
@@ -6,9 +6,6 @@
 #include <unordered_map>
 #include <unordered_set>
 
-int in_list = 0;
-int in_function_body = 0;
-
 enum TokenType {
     INTEGER, CHAR, LIST, STRING, BOOLEAN, FUNCTION, INDEX,
     FUNCTION_ARG,
@@ -34,13 +31,6 @@ struct list_element {
     std::vector<list_element> list;
 };
 
-std::unordered_map<std::string, int> variables_integer;
-std::unordered_map<std::string, char> variables_char;
-std::unordered_map<std::string, std::vector<list_element>> variables_list;
-std::unordered_map<std::string, TokenType> variables_type;
-std::unordered_set<std::string> already_declared;
-std::unordered_map<std::string, std::vector<std::string>> function_arguments;
-
 struct Token {
     TokenType type;
     char character;
@@ -48,11 +38,6 @@ struct Token {
     std::vector<list_element> list;
     std::string name;
 };
-
-std::vector<Token> tokens;
-std::unordered_map<std::string, std::vector<Token>> function_body;
-std::vector<Token> cur_function_body;
-std::string cur_function_name;
 
 struct EvaluateValue {
     TokenType type;
